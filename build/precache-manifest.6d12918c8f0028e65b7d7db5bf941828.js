ce35b794cc48cad5b455699188eda66546489070
yangwen <developer@yangwen.local>
2020-10-23 10:35:33 +0800

    [add]bug

---
+++ build/precache-manifest.6d12918c8f0028e65b7d7db5bf941828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe46803e65fcf5708a1831130e23d808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "299c404e26ee09674bf2",
+    "url": "/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "604b02570dce209dbf08",
+    "url": "/static/css/main.49d9d956.chunk.css"
+  },
+  {
+    "revision": "299c404e26ee09674bf2",
+    "url": "/static/js/2.dbf06d80.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.dbf06d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604b02570dce209dbf08",
+    "url": "/static/js/main.d13e47b4.chunk.js"
+  },
+  {
+    "revision": "674838a540ffc7faaf94",
+    "url": "/static/js/runtime-main.d68f5756.js"
+  }
+]);
