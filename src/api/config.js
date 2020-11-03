208d9f63059a2f59a24ec18c564bb1bb45afd313
18281578906 <1454040353@qq.com>
2020-11-03 20:07:37 +0800

    [mod]bug

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
 // baseURL = 'http://103.218.240.32'
-export const baseURL = 'http://119.27.178.70:9501'  //接口地址
+export const baseURL = 'http://192.168.16.39:9501'  //接口地址
 export const timeout = 10000                    //请求延迟
