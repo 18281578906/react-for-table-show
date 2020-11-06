3b3a14f34f19d9e867ec64af511ebd4af579a5be
yangwen <developer@yangwen.local>
2020-11-06 10:48:38 +0800

    [bug]

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 // baseURL = 'http://103.218.240.32'
-export const baseURL = 'http://119.27.178.70:9501' //测试
-    // export const baseURL = 'http://192.168.16.39:9501' //线上
+// export const baseURL = 'http://119.27.178.70:9501' //测试
+export const baseURL = 'http://192.168.16.39:9501' //线上
 export const timeout = 10000 //请求延迟
