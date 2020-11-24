06b3de52b4b737ff26488f37d09a00e9c476c561
yangwen <yangwen@yangwendeMacBook-Pro.local>
2020-11-24 16:40:18 +0800

    [build]

---
+++ build/precache-manifest.414db5cce15b5ec8873072008da24d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e9ef1563a0bbbe2d516f3cd4772227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c63a2802ad0167a0c590",
+    "url": "/static/css/2.dc7f1509.chunk.css"
+  },
+  {
+    "revision": "723c87b61a60423a11bc",
+    "url": "/static/css/main.1c10bcfe.chunk.css"
+  },
+  {
+    "revision": "c63a2802ad0167a0c590",
+    "url": "/static/js/2.de2f836d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.de2f836d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723c87b61a60423a11bc",
+    "url": "/static/js/main.ac79f9a3.chunk.js"
+  },
+  {
+    "revision": "674838a540ffc7faaf94",
+    "url": "/static/js/runtime-main.d68f5756.js"
+  }
+]);
